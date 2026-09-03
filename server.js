888afcdd9557317b5bfb1e3590112167b64eb7e1
Karim Mohammed <karemkarem201465@gmail.com>
2026-09-03 14:51:45 +0300

    .

--- server.js
+++ server.js
@@ -96,6 +96,10 @@ app.use(
 );
 app.use("/api/shop/review", authMiddleware, shopReviewRouter);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server is now rounning on port 5000");
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+module.exports = app;
